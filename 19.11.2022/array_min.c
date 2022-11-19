32e914ebf0b9843d42765c0285098d7a92ac2129
Oksuzova <oksuzova@gmail.com>
2022-11-19 18:56:22 +0100

    22/11/2022

--- 19.11.2022/array_min.c
+++ 19.11.2022/array_min.c
@@ -3,23 +3,25 @@
 int main()
 {
     int mas[5];
-    int min = 1000;
+    int min = 1000, n;
 
     printf("Entering an array of integers\n");
     printf("After entering each number, press Enter.\n");
 
     int i = 0;
 
-    for (; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
         printf("mas[%d] -> ", i+1);
         scanf("%i", &mas[i]);
-        if (mas[i] < min)
+        if (mas[i] < min){
         min = mas[i];
+        n = i+1;
+        }
 
     }
 
-    printf("\nThe minimum element of the array: mas[%d]=%d", i-1, min);
+    printf("\nThe minimum element of the array: mas[%d]=%d", n, min);
 
     return 0;
 }
