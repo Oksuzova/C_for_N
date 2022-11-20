3893baf38019d367e7982b29b220abe6f8e76947
Oksuzova <oksuzova@gmail.com>
2022-11-20 18:32:32 +0100

    20/11/2022

---
+++ 20.11.2022/arith_mean_p.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main()
+{
+    int a[10];
+    float arithm_mean;
+    int counter = 0, sum = 0;
+    int *p;
+
+
+    printf("Arithmetic mean of non-zero elements.\n");
+    printf("Enter the array elements in the one line,\n");
+    printf(" 10 numbers and press enter\n -> ");
+
+    p = a;
+    for (int i = 1; i <= 10; i++)
+        scanf("%i", p++);
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (*p != 0) counter++;
+        sum += *p;
+        p++;
+    }
+
+    arithm_mean = (float) sum/counter;
+
+    printf("The sum of the elements of the array: %d\n", sum);
+    printf("Number of non-zero elements: %d\n", counter);
+    printf("Arithmetic mean of non-zero elements: %f\n", arithm_mean);
+
+
+}
