40299ccb5cd59ece248bf2a3a02e0e07d95b1785
Oksuzova <oksuzova@gmail.com>
2022-11-21 19:08:12 +0100

    21/11/2022

---
+++ 21.11.2022/bubble_sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void main()
+{
+    const int len = 10;
+    int mas[len];
+    int max, tmp, check;
+
+
+    printf("Descending sort\n");
+    printf("Enter %d numbers.\n", len);
+
+    for (int i = 0; i < len; i++)
+    {
+        printf(" -> ");
+        scanf("%i", &mas[i]);
+    }
+
+    for (int i = len-1; i >= 0; i--)
+    {
+        check = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (mas[j] < mas[j+1])
+            {
+                tmp = mas[j];
+                mas[j] = mas[j+1];
+                mas[j+1] = tmp;
+                check = 0;
+                }
+        }
+        if (check == 1)
+            break;
+    }
+
+    printf("\nSorted array: ");
+
+    for (int i = 0; i < len; i++)
+        printf(" %d ", mas[i]);
+
+}
+
